0e53e1f69665d117d81b967ca9e4b8537224432e
KielGanteng <hendy300408@gmail.com>
2025-08-20 22:10:18 +0700

    Feat: Add App.php on bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        //Comment For Middleware
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
